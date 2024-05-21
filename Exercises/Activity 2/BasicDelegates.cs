bb6a745c61fda3f2df64a734a8c7d2a3eca3f143
Bryan Simón Polo Orellana <102623208+bspoloo@users.noreply.github.com>
2024-05-21 19:15:47 -0400

    Added Activity 2 about Delegates basics and Multicast

---
+++ Exercises/Activity 2/BasicDelegates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_5_backend.Exercises.Activity_2
+{
+    public class BasicDelegates
+    {
+        delegate int MathOperation(int a, int b);
+
+        public BasicDelegates()
+        {
+
+        }
+        public int Add(int a, int b)
+        {
+            Console.WriteLine($"Add: {a} + {b} = {a + b}");
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            Console.WriteLine($"Subtract: {a} - {b} = {a - b}");
+            return a - b;
+        }
+        public void Run()
+        {
+            MathOperation operation = Add;
+
+            int addResult = operation(5, 5);
+            Console.WriteLine($"The result add is: {addResult}");
+
+            operation = Subtract;
+            int subtractResult = operation(10, 5);
+            Console.WriteLine($"The result subtraction is: {subtractResult}");
+        }
+    }
+}
---
+++ Exercises/Activity 2/MulticastDelegates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_5_backend.Exercises.Activity_2
+{
+    public class MulticastDelegates
+    {
+        BasicDelegates basicDelegates;
+        delegate int MathOperation(int a, int b);
+
+        public MulticastDelegates()
+        {
+            basicDelegates = new BasicDelegates();
+        }
+        public void Run()
+        {
+            MathOperation operations = basicDelegates.Subtract;
+            operations += basicDelegates.Add;
+
+            int result = operations(10, 5);
+            Console.WriteLine($"The result is: {result}");
+        }
+
+    }
+}
